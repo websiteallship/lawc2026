56a0409feb9f0b46e97d257b29d1ed6dc3374a7d
websiteallship <website.allship@gmail.com>
2026-06-16 18:05:35 +0700

    fix: remove invalid global throttle that caused 500 error

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,6 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->trustProxies(at: '*');
-        $middleware->append('throttle:global');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
