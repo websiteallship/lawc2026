b832c8320090a0c80105c16e2f69b100031cfd90
websiteallship <website.allship@gmail.com>
2026-06-20 12:02:00 +0700

    fix(admin-bxh): configure custom tailwind theme for admin panel to fix css on custom blade views

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -40,6 +40,7 @@ class AdminPanelProvider extends PanelProvider
             ->colors([
                 'primary' => Color::Amber,
             ])
+            ->viteTheme('resources/css/filament/admin/theme.css')
             ->databaseNotifications()
             ->renderHook(
                 \Filament\View\PanelsRenderHook::FOOTER,
